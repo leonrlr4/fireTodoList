43f05eccc13f80c48bbbbce6fce6956247fa5e48
leonrlr4 <leonrlr4@gmail.com>
2019-03-20 14:57:53 +0800

    make source more secure

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDaau21kTYct_IvxIXahTQyw11Y8O5Xivk",
+    authDomain: "test-2fa78.firebaseapp.com",
+    databaseURL: "https://test-2fa78.firebaseio.com",
+    projectId: "test-2fa78",
+    storageBucket: "test-2fa78.appspot.com",
+    messagingSenderId: "694391532523"
+};
+firebase.initializeApp(config);
